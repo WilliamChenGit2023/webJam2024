a1b0b6e01fe525a23ac7db114fd6dc2ab1a1f1c9
Canaan Zhu <jianaz11@uci.edu>
2024-11-19 23:22:32 -0800

    Fixed Stuff (hekp)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,40 @@ function App() {
   return (
     <div className="header">
       <motion.div 
-      animate={{y: -100, scale: 1}}
+      animate={{y: 100, scale: 1}}
       initial={{scale: 0}}
       transition={{type: "spring"}}>
         Washer Vision
       </motion.div>
+      <PlaceHolder></PlaceHolder>
     </div>
   );
+}
+function PlaceHolder(){
+  return(
+  <div id = "image_placeholder">
+  <form action = "/action_page.php" enctype="multipart/form-data">
+    <lable for = "myfile">Please Film the Washing Machine (please do not delete)</lable>
+    <input
+      type = "file"
+      id = "environment"
+      capture="environment"
+      accept="Video/*"
+    />
+  </form>
+  <br/>
+  <form action = "/action_page.php" enctype="multipart/form-data">
+    <lable for = "myfile">Please Take Pictures of the Washing Machine (please do not delete for now)</lable>
+    <input
+      type = "file"
+      id = "environment"
+      capture="environment"
+      accept="Picture/*"
+    />
+  </form>
+  <br/>
+  </div>
+  )
 }
 
 export default App;
