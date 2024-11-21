0a22c9fb70d4cc9caf078c2c1b3a0d8116b72d77
Canaan Zhu <jianaz11@uci.edu>
2024-11-20 20:34:32 -0800

    CREDITS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,6 +16,10 @@ function App() {
       <PlaceHolder></PlaceHolder>
       <Record></Record>
     </div>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Credits></Credits>
     </div>
   );
 }
@@ -70,5 +74,22 @@ function MyVideoProgram(){
     console.log("The media is not supported")
   }
 }
-
+function Credits(){
+  return(
+    <div id = "ourCredits">
+      <br></br>
+      <p>
+        This Website is Created for the Purpose of Webjam 2024
+        and For the Purpose of Traking Washing Machines
+      </p>
+      <p>
+        Created by Will Bao, Cody Chen, William Chen, Jianan Zhu
+      </p>
+      <p>
+        Programmed in React using Javascript and Python
+      </p>
+      <br></br>
+    </div>
+  )
+}
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Webtest from './Webtest';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <Webtest />
+    <App />
   </React.StrictMode>
 );
