97ae32329b2ed6f6da4cbea27a30547a1b9aabbf
Will-TCD <willb@uci.edu>
2024-11-20 17:50:38 -0800

    website icon
    edited html icon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,19 @@ import { motion } from "framer-motion";
 
 function App() {
   return (
+    <div>
     <div className="header" onLoad = {MyVideoProgram()}>
       <motion.div 
       animate={{y: 100, scale: 1}}
-      initial={{scale: 0}}
+      initial={{y: '-10vw', scale: 0}}
       transition={{type: "spring"}}>
         Washer Vision
       </motion.div>
+    </div>
+    <div>
       <PlaceHolder></PlaceHolder>
       <Record></Record>
+    </div>
     </div>
   );
 }
