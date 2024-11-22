860ef9e7a49667a94b7cd18805727b326f5fa07d
Will-TCD <willb@uci.edu>
2024-11-21 21:45:47 -0800

    fixed gap below video

--- my-app/src/Webtest.js
+++ my-app/src/Webtest.js
@@ -186,7 +186,7 @@ const Webtest = () => {
         <h2>Backend Status</h2>
         <p>{backendStatus ? backendStatus : 'Checking backend...'}</p>
       </div>
-      <div className='main-container'>
+      <div className='main-container2'>
         {!isCameraStarted && (
           <button className='button' onClick={startCamera}>Start Camera</button>
         )}
@@ -210,11 +210,12 @@ const Webtest = () => {
         </select>
       </div>
 
-      <button onClick={toggleRecording}>
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      <button onClick={toggleRecording} className={`button ${isRecording ? "recording" : "not-recording"}`}>
+        {isRecording ? 'Stop Recording ' : 'Start Recording '}
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h480q33 0 56.5 23.5T720-720v180l160-160v440L720-420v180q0 33-23.5 56.5T640-160H160Zm0-80h480v-480H160v480Zm0 0v-480 480Z"/></svg>
       </button>
 
-      <div>
+      <div className='main-container2'>
         <h2>Create New Folder</h2>
         <input 
           type="text" 
