090b9366c0ecdd0161a5b3bc95c1057f3058bb83
edokydocy <edokydocy@gmail.com>
2024-11-21 20:13:48 -0800

    fix cvmain

--- flaskBackend/cvmain.py
+++ flaskBackend/cvmain.py
@@ -9,23 +9,31 @@ from pathlib import Path
 import json
 
 def cropandsaveandprocess(img_path, lx, ty, rx, by):
+    print("&&", lx, ty, rx, by)
     img = cv2.imread(str(img_path))
     grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     invtimg = cv2.bitwise_not(grayimg)
     _, thresholdedimg = cv2.threshold(invtimg, 100, 255, cv2.THRESH_BINARY)
     
-    cv2.imshow("name", invtimg)
-    cv2.waitKey(0)
+    #cv2.imshow("name", thresholdedimg)
+    #print("???? compressed? ", thresholdedimg.shape[:2])
+    #cv2.waitKey(0)
     tmp = thresholdedimg[ty:by, lx:rx]
-    cv2.imwrite("tmp.jpg", tmp)
+    print(ty, by, lx, rx)
+    #cv2.imshow("name", tmp)
+    #cv2.imwrite("./tmp.jpg", tmp)
 
     sum = 0.0
     H = by-ty
     W = rx-lx
-    for i in range(ty, ty+H//4):
-        for j in range(lx, rx):
-            sum += thresholdedimg[i,j]/(H//4)/(rx-lx)
+    for i in range(ty, by):
+        for j in range(lx, lx+W//4):
+            sum += thresholdedimg[i,j]/(W//4)/(by-ty)
     
+    tmp = thresholdedimg[ty:by, lx:lx+W//4]
+    #cv2.imshow("name", tmp)
+    #cv2.imwrite("firstqt.png", tmp)
+
     print("###", sum)
     if sum >= 235:
         return False
@@ -44,6 +52,7 @@ def main(pfold_str: str, if_new_pos = False):
             img_path = it
         else:
             json_path = it
+        print("")
     
     coord_path = Path(f"./uploads/{mach_name}.txt")
 
@@ -56,10 +65,16 @@ def main(pfold_str: str, if_new_pos = False):
         rx = int(json_dic['coordinates'][1]['x'])
         ty = int(json_dic['coordinates'][0]['y'])
         by = int(json_dic['coordinates'][1]['y'])
+    
 
+    if lx>rx:
+        tmp = lx; lx = rx; rx = tmp
+    if ty>by:
+        tmp = ty; ty = by; by = tmp
+    print ("****", lx, ty, rx, by)
     ans = cropandsaveandprocess(img_path, lx, ty, rx, by)
     return ans
 
 
 if __name__=="__main__":
-    print(main("../W1"))
+    print(main("./uploads/D1"))
