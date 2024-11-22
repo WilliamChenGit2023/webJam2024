874a5f3bd410fd836a9fd441fe15484319cb3d9f
WilliamChenGit2023 <129251589+WilliamChenGit2023@users.noreply.github.com>
2024-11-22 15:50:10 -0800

    bruh smasl change

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -2,6 +2,7 @@
 const config = {
     serverAddress: "https://webjambackend-e277cbd6cc5e.herokuapp.com"
     //serverAddress: "https://10.12.141.86:5000"
+    //fun
   };
   
   export default config;
