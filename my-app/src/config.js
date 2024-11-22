a02790476edca2d8aaae50e676bbb7cfb93b9722
WilliamChenGit2023 <129251589+WilliamChenGit2023@users.noreply.github.com>
2024-11-21 20:37:45 -0800

    changed back server

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -1,7 +1,6 @@
 // src/config.js
 const config = {
-    //serverAddress: "https://webjambackend-e277cbd6cc5e.herokuapp.com"
-    serverAddress: "https://10.12.141.86:5000"
+    serverAddress: "https://webjambackend-e277cbd6cc5e.herokuapp.com"
   };
   
   export default config;
